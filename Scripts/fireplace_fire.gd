07ad5a7e731d6f9d240022967621d4bdee709614
Isaias Lourenco <moerbius@gmail.com>
2024-12-31 00:55:53 +0000

    Added Hero Interior

--- Scripts/fireplace_fire.gd
+++ Scripts/fireplace_fire.gd
@@ -1,12 +1,10 @@
 extends Node2D
 
-@onready var spawn = $HeroSpawn
-@onready var hero = $Hero
+@onready var animation = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#hero.position = spawn.position;
-	pass
+	animation.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
